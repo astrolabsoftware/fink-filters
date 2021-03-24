6784010a88df64ab9a51accfa5d7f8047674a6e8
JulietteVl <52038457+JulietteVl@users.noreply.github.com>
2021-03-24 13:25:04 +0000

    Issues/9/k ne filter (#12)
    The current filter consist of the following criteria:
    - threshold on `kn_score`
    - the number of prior detection must be quite low (see `ndethist`)
    - the first detection of the object must be recent (see `jd`, `jdstarthist`)
    - deep learning real/bogus score
    - Star/galaxy extractor

---
+++ fink_filters/filter_kn_candidates/filter.py
@@ -0,0 +1,66 @@
+from pyspark.sql.functions import pandas_udf, PandasUDFType
+from pyspark.sql.types import BooleanType
+
+import pandas as pd
+
+@pandas_udf(BooleanType(), PandasUDFType.SCALAR)
+def kn_candidates(knscore, drb, classtar, jd, jdstarthist, ndethist, cdsxmatch) -> pd.Series:
+    """ Return alerts considered as KN candidates
+    
+    Parameters
+    ----------
+    cdsxmatch: Spark DataFrame Column
+        Column containing the cross-match values
+    drb: Spark DataFrame Column
+        Column containing the Deep-Learning Real Bogus score
+    classtar: Spark DataFrame Column
+        Column containing the sextractor score
+    knscore: Spark DataFrame Column
+        Column containing the kilonovae score
+    jd: Spark DataFrame Column
+        Column containing observation Julian dates at start of exposure [days]
+    jdstarthist: Spark DataFrame Column
+        Column containing earliest Julian dates of epoch corresponding to ndethist [days]
+    ndethist: Spark DataFrame Column
+        Column containing the number of prior detections (with a theshold of 3 sigma)
+    Returns
+    ----------
+    out: pandas.Series of bool
+        Return a Pandas DataFrame with the appropriate flag:
+        false for bad alert, and true for good alert.
+    """
+    
+    high_knscore = knscore.astype(float) > 0.5
+    high_drb = drb.astype(float) > 0.5
+    high_classtar = classtar.astype(float) > 0.4
+    new_detection = jd.astype(float) - jdstarthist.astype(float) < 20
+    small_detection_history = ndethist.astype(float) < 20
+    
+    
+    list_simbad_galaxies = [
+        "galaxy",
+        "Galaxy",
+        "EmG",
+        "Seyfert",
+        "Seyfert_1",
+        "Seyfert_2",
+        "BlueCompG",
+        "StarburstG",
+        "LSB_G",
+        "HII_G",
+        "High_z_G",
+        "GinPair",
+        "GinGroup",
+        "BClG",
+        "GinCl",
+        "PartofG",
+    ]
+    
+    
+    keep_cds = \
+        ["Unknown", "Transient","Fail"] + list_simbad_galaxies
+
+    f_kn = high_knscore & high_drb & high_classtar & new_detection
+    f_kn = f_kn & small_detection_history & cdsxmatch.isin(keep_cds)
+
+    return f_kn
