1df703656b4c4c80e76963bbbb94922cac3edc04
JulietteVl <52038457+JulietteVl@users.noreply.github.com>
2021-04-27 10:30:23 +0200

    Added ecliptic coordinates in hour, degree format.

--- fink_filters/filter_kn_candidates/filter.py
+++ fink_filters/filter_kn_candidates/filter.py
@@ -1,5 +1,5 @@
 # Copyright 2019-2021 AstroLab Software
-# Author: Juliette Vlieghe
+# Authors: Julien Peloton, Juliette Vlieghe
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -121,6 +121,8 @@ def kn_candidates(
             dec = Angle(
                 np.array(dec.astype(float)[f_kn]) * u.degree
             ).deg
+            ra_formatted = Angle(ra*u.hour).to_string(precision=2)
+            dec_formatted = Angle(dec*u.degree).to_string(precision=1)
             delta_jd_first = np.array(
                 jd.astype(float)[f_kn] - jdstarthist.astype(float)[f_kn]
             )
@@ -198,8 +200,8 @@ def kn_candidates(
                 *Measurement (band {}):*\n- Apparent magnitude: {:.2f} ± {:.2f} \n- Rate: {:.2f} mag/day\n
                 """.format(dict_filt[fid[i]], mag[fid[i]], err_mag[fid[i]], rate[fid[i]])
             position_text = """
-                *Position [deg]:*\n- Right ascension:\t {:.7f}\n- Declination:\t\t\t{:.7f}\n- Galactic latitude:\t{:.7f}\n
-                """.format(ra[i], dec[i], b[i])
+                *Position [deg]:*\n-Equatorial coordinates:\n{} {}\n- Right ascension:\t {:.7f}\n- Declination:\t\t\t{:.7f}\n- Galactic latitude:\t{:.7f}\n
+                """.format(ra_formatted[i], dec_formatted[i], dec[i], ra[i], dec[i], b[i])
 
             tns_text = '*TNS:* <https://www.wis-tns.org/search?ra={}&decl={}&radius=5&coords_unit=arcsec|link>'.format(ra[i], dec[i])
             # message formatting
