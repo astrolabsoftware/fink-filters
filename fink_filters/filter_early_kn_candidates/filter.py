Fix error in absolute magnitude computation and target

fink_filters/filter_early_kn_candidates/filter.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 AstroLab Software
+# Copyright 2021 AstroLab Software
 # Author: Juliette Vlieghe
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -166,7 +166,7 @@ def early_kn_candidates(
         for i, row in enumerate(pdf.itertuples()):
             # SkyCoord didn't keep the original indexes
             idx_reduced = idx_mangrove[idxself == i]
-            abs_mag = np.array(row.mag-1-5*np.log10(
+            abs_mag = np.array(row.mag-25-5*np.log10(
                 pdf_mangrove.loc[idx_reduced, :].lum_dist))
 
             candidates_number = np.argwhere(np.array(
@@ -175,7 +175,7 @@ def early_kn_candidates(
                             dec=row.dec*u.degree
                         ).separation(catalog_mangrove[idx_reduced]).radian
                             < 0.01/pdf_mangrove.loc[idx_reduced, :].ang_dist)
-                        & (abs_mag > 15) & (abs_mag < 17)
+                        & (abs_mag > -15) & (abs_mag < -17)
             ))
             galaxy_matching.append(len(candidates_number) > 0)
 

fink_filters/filter_kn_candidates/filter.py
@@ -123,7 +123,8 @@ def kn_candidates(
             ).deg
             ra_formatted = Angle(ra*u.degree).to_string(precision=2, sep=' ',
                                                         unit=u.hour)
-            dec_formatted = Angle(dec*u.degree).to_string(precision=1, sep=' ')
+            dec_formatted = Angle(dec*u.degree).to_string(precision=1, sep=' ',
+                                                          alwayssign=True)
             delta_jd_first = np.array(
                 jd.astype(float)[f_kn] - jdstarthist.astype(float)[f_kn]
             )
@@ -201,7 +202,7 @@ def kn_candidates(
                 *Measurement (band {}):*\n- Apparent magnitude: {:.2f} ± {:.2f} \n- Rate: {:.2f} mag/day\n
                 """.format(dict_filt[fid[i]], mag[fid[i]], err_mag[fid[i]], rate[fid[i]])
             radec_text = """
-                 *RA/Dec:*\n- [hours, deg]: {} {}\n- [deg, deg]: {:.7f} {:.7f}
+                 *RA/Dec:*\n- [hours, deg]: {} {}\n- [deg, deg]: {:.7f} {:+.7f}
                  """.format(ra_formatted[i], dec_formatted[i], ra[i], dec[i])
             galactic_position_text = """
                 *Galactic latitude:*\n- [deg]: {:.7f}""".format(b[i])