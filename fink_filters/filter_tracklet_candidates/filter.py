837c7438c6f1451faf5f01b1c25b4e8b69c5e751
Julien <peloton@lal.in2p3.fr>
2022-01-18 15:24:05 +0100

    Update the condition to flag tracklets

--- fink_filters/filter_tracklet_candidates/filter.py
+++ fink_filters/filter_tracklet_candidates/filter.py
@@ -40,7 +40,7 @@ def tracklet_candidates_(tracklet) -> pd.Series:
 
     >>> assert 'ZTF21acqersq' in pdf[classification]['objectId'].values
     """
-    f_tracklet = tracklet.apply(lambda x: (x != None) & (x.startswith('TRCK_')))
+    f_tracklet = tracklet.apply(lambda x: (not isinstance(x, type(None))) & (x.startswith('TRCK_')))
 
     return f_tracklet
 
