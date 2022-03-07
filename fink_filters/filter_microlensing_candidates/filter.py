cfaf69b1bec77578fb6e2a66ed098e2cf0a62249
Julien <peloton@lal.in2p3.fr>
2022-03-07 14:42:40 +0100

    update the mulens filter

--- fink_filters/filter_microlensing_candidates/filter.py
+++ fink_filters/filter_microlensing_candidates/filter.py
@@ -15,6 +15,8 @@
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 from pyspark.sql.types import BooleanType
 
+from fink_filters.tester import spark_unit_tests
+
 import pandas as pd
 
 def microlensing_candidates_(mulens) -> pd.Series:
@@ -62,6 +64,14 @@ def microlensing_candidates(mulens) -> pd.Series:
         Return a Pandas DataFrame with the appropriate flag:
         false for bad alert, and true for good alert.
 
+    Examples
+    ----------
+    >>> from fink_filters.utilities import apply_user_defined_filter
+    >>> df = spark.read.format('parquet').load('datatest')
+    >>> f = 'fink_filters.filter_microlensing_candidates.filter.microlensing_candidates'
+    >>> df = apply_user_defined_filter(df, f)
+    >>> print(df.count())
+    0
     """
     f_mulens = microlensing_candidates_(
         mulens
@@ -72,12 +82,7 @@ def microlensing_candidates(mulens) -> pd.Series:
 
 if __name__ == "__main__":
     """ Execute the test suite """
-    import sys
-    import doctest
-    import numpy as np
 
-    # Numpy introduced non-backward compatible change from v1.14.
-    if np.__version__ >= "1.14.0":
-        np.set_printoptions(legacy="1.13")
-
-    sys.exit(doctest.testmod()[0])
+    # Run the test suite
+    globs = globals()
+    spark_unit_tests(globs)
