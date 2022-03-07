40d9b16dd7ef6e06312452f581cf792aff9f0adb
Julien <peloton@lal.in2p3.fr>
2022-03-07 14:32:34 +0100

    Remove unused code

--- fink_filters/tester.py
+++ fink_filters/tester.py
@@ -16,41 +16,6 @@ import sys
 import doctest
 import numpy as np
 
-def regular_unit_tests(global_args: dict = None, verbose: bool = False):
-    """ Base commands for the regular unit test suite
-
-    Include this routine in the main of a module, and execute:
-    python3 mymodule.py
-    to run the tests.
-
-    It should exit gracefully if no error (exit code 0),
-    otherwise it will print on the screen the failure.
-
-    Parameters
-    ----------
-    global_args: dict, optional
-        Dictionary containing user-defined variables to
-        be passed to the test suite. Default is None.
-    verbose: bool
-        If True, print useful debug messages.
-        Default is False.
-
-    Examples
-    ----------
-    Set "toto" to "myvalue", such that it can be used during tests:
-    >>> globs = globals()
-    >>> globs["toto"] = "myvalue"
-    >>> regular_unit_tests(global_args=globs)
-    """
-    if global_args is None:
-        global_args = globals()
-
-    # Numpy introduced non-backward compatible change from v1.14.
-    if np.__version__ >= "1.14.0":
-        np.set_printoptions(legacy="1.13")
-
-    sys.exit(doctest.testmod(globs=global_args, verbose=verbose)[0])
-
 def spark_unit_tests(global_args: dict = None, verbose: bool = False):
     """ Base commands for the Spark unit test suite
 
