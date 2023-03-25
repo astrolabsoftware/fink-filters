742e068219608e46322c97d0940104d255c950aa
Timofei Pshenichniy <timofei.psheno@gmail.com>
2023-03-25 22:58:19 +0300

    Comments added

--- fink_filters/filter_anomaly_notification/filter.py
+++ fink_filters/filter_anomaly_notification/filter.py
@@ -9,9 +9,43 @@ import pandas as pd
 
 
 def anomaly_notification_(df, threshold=10):
+     """
+    Create event notifications with a high anomaly_score value
+    Parameters
+    ----------
+    df : Spark DataFrame with column :
+        objectId : unique identifier for this object
+        lc_features: Dict of dicts of floats. Keys of first dict - filters (fid), keys of inner dicts - names of features
+        rb: RealBogus quality score
+        anomaly_score: Anomaly score
+        timestamp : UTC time
+    threshold : Number of notifications (10 by default)
+    
+    Returns
+    ----------
+    out: pandas.Series of bool    
+    Return a Pandas DataFrame with the appropriate flag:
+    false for bad alert, and true for good alert.
+    
+    Examples
+    ----------
+    >>> from fink_utils.spark.utils import concat_col
+    >>> from fink_science.ad_features.processor import extract_features_ad
+    >>> from fink_science.anomaly_detection.processor import anomalys_score
+    >>> df = spark.read.format('parquet').load('datatest')
+    >>> what = ['magpsf', 'jd', 'sigmapsf', 'fid']
+    >>> prefix = 'c'
+    >>> what_prefix = [prefix + i for i in what]
+    >>> for colname in what:
+    ...    df = concat_col(df, colname, prefix=prefix)
+    >>> df = df.withColumn('lc_features', extract_features_ad(*what_prefix, 'objectId'))
+    >>> df = df.withColumn("anomaly_score", anomaly_score("lc_features"))
+    >>> mask = anomaly_notification_(df.select('objectId', 'candidate.ra', 'candidate.dec', 'candidate.rb', 'anomaly_score', 'timestamp'))
+    >>> print(sum(mask))
+    """
     med = df.select('anomaly_score').approxQuantile('anomaly_score', [0.5], 0.25)
     df_filtred = df.sort(['anomaly_score'], ascending=True).limit(min(df.count(), threshold))
-    df_min = df_filtred.toPandas()
+    df_min = df_filtred.toPandas() #Only 10-20 objects fall into the Pandas dataframe
     filtred_ID = set(df_min['objectId'].values)
     df = df.withColumn('flag', 
                        when((df.objectId.isin(filtred_ID)), lit(True))
