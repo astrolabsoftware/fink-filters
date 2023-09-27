e5db2cb79ded70803871cf60cd368b90bab76c5f
Roman <roman.lemontagner@gmail.com>
2023-09-27 16:28:13 +0200

    fix condition

--- fink_filters/filter_svom_ft_tom/filter.py
+++ fink_filters/filter_svom_ft_tom/filter.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from fink_filters.classification import extract_fink_classification_
 from fink_utils.xmatch.simbad import return_list_of_eg_host
 
@@ -5,24 +7,71 @@ from astropy.coordinates import SkyCoord
 
 
 def gvom_filter(
-    rb,
-    magpsf,
-    ra,
-    dec,
-    cdsxmatch,
-    roid,
-    mulens,
-    snn_snia_vs_nonia,
-    snn_sn_vs_all,
-    rf_snia_vs_nonia,
-    ndethist,
-    drb,
-    classtar,
-    jd,
-    jdstarthist,
-    rf_kn_vs_nonkn,
-    tracklet,
-):
+    rb: pd.Series,
+    magpsf: pd.Series,
+    ra: pd.Series,
+    dec: pd.Series,
+    cdsxmatch: pd.Series,
+    roid: pd.Series,
+    mulens: pd.Series,
+    snn_snia_vs_nonia: pd.Series,
+    snn_sn_vs_all: pd.Series,
+    rf_snia_vs_nonia: pd.Series,
+    ndethist: pd.Series,
+    drb: pd.Series,
+    classtar: pd.Series,
+    jd: pd.Series,
+    jdstarthist: pd.Series,
+    rf_kn_vs_nonkn: pd.Series,
+    tracklet: pd.Series,
+)-> pd.Series:
+    """
+    Science filter of the gvom network.
+    Select alerts that are likely to be fast transient.
+
+    Parameters
+    ----------
+    rb : pd.Series
+        real bogus
+    magpsf : pd.Series
+        magnitude
+    ra : pd.Series
+        right ascension
+    dec : pd.Series
+        declination
+    cdsxmatch : pd.Series
+        cds tag
+    roid : pd.Series
+        asteroid tag
+    mulens : pd.Series
+        micro-lensing score
+    snn_snia_vs_nonia : pd.Series
+        supernovae Ia score
+    snn_sn_vs_all : pd.Series
+        supernovae all score
+    rf_snia_vs_nonia : pd.Series
+        supernovae Ia score
+    ndethist : pd.Series
+        number of detection in the history
+    drb : pd.Series
+        deep learning real bogus
+    classtar : pd.Series
+        Sextractor score
+    jd : pd.Series
+        julian date of the alerts
+    jdstarthist : pd.Series
+        start history julian date
+    rf_kn_vs_nonkn : pd.Series
+        kilonovae score
+    tracklet : pd.Series
+        satellite identification
+
+    Returns
+    -------
+    pd.Series
+        if True, the alert match the gvom science filter
+    """
+    
     classification = extract_fink_classification_(
         cdsxmatch,
         roid,
@@ -58,13 +107,13 @@ def gvom_filter(
     gal_latitude = coord.galactic.b
     mask_south_gal = gal_latitude < -15
     mask_north_gal = gal_latitude > 15
-    f_gal = mask_north_gal & mask_south_gal
+    f_gal = mask_north_gal | mask_south_gal
 
     # alerts not in the ecliptic
     ecl_latitude = coord.transform_to("geocentricmeanecliptic").lat
     mask_south_ecl = ecl_latitude < -15
     mask_north_ecl = ecl_latitude > 15
-    f_ecl = mask_north_ecl & mask_south_ecl
+    f_ecl = mask_north_ecl | mask_south_ecl
 
     # bright alerts
     f_brightness = magpsf <= 17.0
