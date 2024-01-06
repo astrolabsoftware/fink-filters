e25f4844ba4218fffe679d92b96ada2715476b7a
JulienPeloton <peloton@lal.in2p3.fr>
2024-01-06 16:21:08 +0100

    Missing import

--- fink_filters/filter_anomaly_notification/filter.py
+++ fink_filters/filter_anomaly_notification/filter.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import pandas as pd
 import numpy as np
 
 from astropy import units as u
