b5be91e699cfcf726aeb918f1d0a8cb264e9e536
Julian Hamo <hamo@ijclab.in2p3.fr>
2025-01-17 11:56:43 +0100

    Add low state blazar filter example

--- fink_filters/filter_blazar_low_state/filter.py
+++ fink_filters/filter_blazar_low_state/filter.py
@@ -1,3 +1,19 @@
+# Copyright 2021-2022 AstroLab Software
+# Author: Julian Hamo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from line_profiler import profile
+
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 from pyspark.sql.types import BooleanType
 
@@ -8,8 +24,8 @@ from fink_filters.tester import spark_unit_tests
 
 
 @pandas_udf(BooleanType(), PandasUDFType.SCALAR)
-def low_state_filter(flux_state: pd.Series) -> pd.Series:
-    # CHange argument to only flux_state column ?
+@profile
+def low_state_filter(flux_state) -> pd.Series:
     """Returns True the alert is considered a low state,
        returns False else.
 
@@ -20,14 +36,27 @@ def low_state_filter(flux_state: pd.Series) -> pd.Series:
 
     Returns
     -------
-    check: np.ndarray
+    check: pd.Series
         Mask that returns True if the alert is a low state,
         False else
+
+    Examples
+    --------
+    >>> from fink_science.blazar_low_state.processor import quiescent_state
+    >>> from fink_utils.spark.utils import apply_user_defined_filter
+
+    # Test
+    >>> df = spark.read.parquet(ztf_alert_sample)
+    >>> f = 'fink_filters.filter_blazar_low_state.filter.low_state_filter'
+    >>> df = apply_user_defined_filter(df, f)
+    >>> print(df.count())
+    1
     """
 
     tmp = np.array(flux_state.toPandas().values.tolist())
     tmp = tmp.reshape(tmp.shape[0], tmp.shape[-1]).transpose()
     tmp[pd.isnull(tmp)] = np.nan
+    tmp[tmp < 0] = np.nan
     return pd.Series((tmp[1] < 1) & (tmp[2] < 1))
 
 
@@ -36,4 +65,10 @@ if __name__ == "__main__":
 
     # Run the test suite
     globs = globals()
+    path = os.path.join(os.path.dirname(__file__), 'datatest/blazars')
+    filename = 'CTAO_blazar_datatest_v20-12-24.parquet'
+    ztf_alert_sample = "file://{}/{}".format(path, filename)
+    globs["ztf_alert_sample"] = ztf_alert_sample
+
+    # Run the test suite
     spark_unit_tests(globs)
