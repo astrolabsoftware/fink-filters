f8ffb2cf180e8d6b791826327817ff387f75b619
Emille Ishida <emilleishida@users.noreply.github.com>
2025-09-01 02:51:16 -0300

    YSO - apply a series of cuts required by the user (#257)
    * include slope cut
    
    * add data for yso cut tests
    
    * add fix to SPICY filter
    
    * add modifications to yso filter
    
    * call linear fit properly on test
    
    * linting
    
    * Display the entire light curve in the bot instead of only 30 days
    
    ---------
    
    Co-authored-by: Emile <emile@clrlsstpo04.in2p3.fr>
    Co-authored-by: JulienPeloton <peloton@lal.in2p3.fr>

--- fink_filters/ztf/livestream/filter_yso_spicy_candidates/filter.py
+++ fink_filters/ztf/livestream/filter_yso_spicy_candidates/filter.py
@@ -21,13 +21,73 @@ from fink_utils.tg_bot.utils import get_curve, msg_handler_tg
 
 from fink_filters.tester import spark_unit_tests
 
+import numpy as np
 import pandas as pd
 import os
 
 
+def r2_score(jd, magpsf, fid, min_points=5):
+    """
+    Compute the coefficient of determination (R²) for two numeric arrays.
+
+    Parameters
+    ----------
+    jd : array-like
+        Independent variable (predictor values). Time of observation.
+    magpsf : array-like
+        Dependent variable (observed values). PSF magnitude.
+    fid: array-like
+        Observed filter. For ZTF, g=1, r=2
+    min_points: int
+        Minimum number of points required.
+
+    Returns
+    -------
+    float
+        R² value.
+    """
+    # remove Nans
+    mask_nan = ~np.isnan(magpsf)
+    mask_r = fid == 2
+    mask = mask_nan & mask_r
+
+    if sum(mask) < min_points:
+        return np.nan
+
+    x = jd[mask]
+    y = magpsf[mask]
+
+    x_mean = x.mean()
+    y_mean = y.mean()
+
+    sxx = np.sum(pow(x - x_mean, 2))
+    if sxx == 0:
+        return np.nan
+
+    sxy = np.sum((x - x_mean) * (y - y_mean))
+    beta1 = sxy / sxx
+    beta0 = y_mean - beta1 * x_mean
+
+    y_hat = beta0 + beta1 * x
+    ss_res = np.sum(pow(y - y_hat, 2))
+    ss_tot = np.sum(pow(y - y_mean, 2))
+
+    res = 1.0 - ss_res / ss_tot if ss_tot != 0 else np.nan
+
+    return res
+
+
 @pandas_udf(BooleanType(), PandasUDFType.SCALAR)
 def yso_spicy_candidates(
-    spicy_id, spicy_class, objectId, cjdc, cmagpsfc, csigmapsfc, cdiffmaglimc, cfidc
+    spicy_id,
+    spicy_class,
+    objectId,
+    cjdc,
+    cmagpsfc,
+    csigmapsfc,
+    cdiffmaglimc,
+    cfidc,
+    linear_fit_slope,
 ) -> pd.Series:
     """Return alerts with a match in the SPICY catalog
 
@@ -47,7 +107,9 @@ def yso_spicy_candidates(
     --------
     >>> from fink_utils.spark.utils import apply_user_defined_filter
     >>> from fink_utils.spark.utils import concat_col
+    >>> import pyspark.sql.functions as F
     >>> df = spark.read.format('parquet').load('datatest/spicy_yso')
+    >>> df = df.withColumn('linear_fit_slope', F.col('lc_features_r.linear_fit_slope'))
 
     >>> to_expand = ['jd', 'fid', 'magpsf', 'sigmapsf', 'diffmaglim']
 
@@ -62,10 +124,12 @@ def yso_spicy_candidates(
     >>> f = 'fink_filters.ztf.livestream.filter_yso_spicy_candidates.filter.yso_spicy_candidates'
     >>> df = apply_user_defined_filter(df, f)
     >>> print(df.count())
-    10
+    6
     """
-    mask = spicy_id.to_numpy() != -1
+    slope_lim = 0.025  # minimum slope threshold
+    r2_lim = 0.6  # minimum required r2
 
+    # convert to pandas
     pdf = pd.DataFrame({
         "objectId": objectId,
         "magpsf": cmagpsfc,
@@ -75,20 +139,30 @@ def yso_spicy_candidates(
         "jd": cjdc,
         "spicy_id": spicy_id,
         "spicy_class": spicy_class,
+        "linear_fit_slope": linear_fit_slope,
     })
+
+    # select spicy objecs, N
+    mask_spicy = spicy_class != "Unknown"
+
+    # select spicy objects which respect the slope threshold, N
+    mask_slope = mask_spicy & (linear_fit_slope.abs() > slope_lim)
+
+    # calculate r2 statistics
+    pdf["r2_values"] = pdf[["jd", "magpsf", "fid"]].apply(
+        lambda vecs: r2_score(*vecs), axis=1
+    )
+    mask_r2 = pdf["r2_values"] > r2_lim
+
+    mask = mask_r2 & mask_slope
 
     # Loop over matches
     if ("FINK_TG_TOKEN" in os.environ) and os.environ["FINK_TG_TOKEN"] != "":
         payloads = []
         for _, alert in pdf[mask].iterrows():
             curve_png = get_curve(
-                jd=alert["jd"],
-                magpsf=alert["magpsf"],
-                sigmapsf=alert["sigmapsf"],
-                diffmaglim=alert["diffmaglim"],
-                fid=alert["fid"],
                 objectId=alert["objectId"],
-                origin="fields",
+                origin="API",
             )
 
             hyperlink = "[{}](https://fink-portal.org/{}): ID {} ({})".format(
@@ -102,7 +176,7 @@ def yso_spicy_candidates(
         if len(payloads) > 0:
             msg_handler_tg(payloads, channel_id="@spicy_fink", init_msg="")
 
-    return pd.Series(mask)
+    return mask
 
 
 if __name__ == "__main__":
@@ -110,4 +184,7 @@ if __name__ == "__main__":
 
     # Run the test suite
     globs = globals()
+    path = os.path.dirname(__file__)
+    sample_file = "./fink-filters/datatest/spicy_yso/"
+    globs["test_yso_cuts"] = sample_file
     spark_unit_tests(globs)
