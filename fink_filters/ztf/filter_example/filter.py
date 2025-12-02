300c2fe531d2e7ea5c1ac88659acdfe3cee4e939
Emille Ishida <emilleishida@users.noreply.github.com>
2025-12-02 06:29:45 -0600

    change data and output from testing (#283)
    * change data and output from testing
    
    * add example inside fink_filters
    
    * remove intermediary file
    
    * fixed output from testing
    
    * fixed typo on file name calling
    
    * returned the tutorial to its original form, changed only the data test

--- fink_filters/ztf/filter_example/filter.py
+++ fink_filters/ztf/filter_example/filter.py
@@ -19,13 +19,11 @@ from fink_filters.tester import spark_unit_tests
 
 import pandas as pd
 
-def example_filter_(cdsxmatch, magpsf) -> pd.Series:
-    """ Return alerts with counterpart in the SIMBAD database with difference magnitude above 20
+def example_filter_(magpsf) -> pd.Series:
+    """ Return alerts with difference magnitude above 18
 
     Parameters
     ----------
-    cdsxmatch: Pandas series
-        Column containing the SIMBAD cross-match values
     magpsf: Pandas series
         Column containing difference magnitudes
         
@@ -37,41 +35,33 @@ def example_filter_(cdsxmatch, magpsf) -> pd.Series:
 
     Examples
     ----------
-    >>> pdf = pd.read_parquet('datatest/magnetic_cvs/')
-    >>> classification = example_filter_(pdf['cdsxmatch'], pdf['candidate'].apply(lambda x: x['magpsf']))
+    >>> pdf = pd.read_parquet('datatest/regular/')
+    >>> classification = example_filter_(pdf['candidate'].apply(lambda x: x['magpsf']))
     >>> nalerts = len(pdf[classification]['objectId'])
     >>> print(nalerts)
-    1
+    169
 
     >>> pdf[classification].groupby('cdsxmatch').count().sort_values('objectId', ascending=False)['objectId'].head()
     cdsxmatch
-    QSO    1
+    LongPeriodV*    16
+    EclBin           8
+    Unknown          8
+    delSctV*         7
+    Mira             7
     Name: objectId, dtype: int64
     """
-    myfilter = ~cdsxmatch.isin(['Unknown', 'Transient'])
-
-    # mask all kind of failures
-    mask = cdsxmatch.apply(lambda x: x.startswith('Fail'))
-    myfilter[mask] = False
-
-    # Remove static objects -- https://github.com/astrolabsoftware/fink-filters/issues/120
-    mask_gal = cdsxmatch.apply(lambda x: x.startswith('Galaxy'))
-    myfilter[mask_gal] = False
     
-    mask_mag = magpsf <= 20.5
-    myfilter[mask_mag] = False
+    mask_mag = magpsf <= 18
     
-    return myfilter
+    return mask_mag
 
 
 @pandas_udf(BooleanType())
-def example_filter(cdsxmatch: pd.Series, magpsf: pd.Series) -> pd.Series:
+def example_filter(magpsf: pd.Series) -> pd.Series:
     """ Pandas UDF version of example_filter_ for Spark
 
     Parameters
     ----------
-    cdsxmatch: Spark DataFrame Column
-        Column containing the SIMBAD cross-match values
     magpsf: Spark DataFrame Column
         Column containing the difference magnitudes
 
@@ -84,16 +74,15 @@ def example_filter(cdsxmatch: pd.Series, magpsf: pd.Series) -> pd.Series:
     Examples
     ----------
     >>> from fink_utils.spark.utils import apply_user_defined_filter
-    >>> df = spark.read.format('parquet').load('datatest/magnetic_cvs/')
-    >>> f = 'fink_filters.ztf.filter_bright.filter.example_filter'
+    >>> df = spark.read.format('parquet').load('datatest/regular/')
+    >>> f = 'fink_filters.ztf.filter_example.filter.example_filter'
     >>> df = apply_user_defined_filter(df, f)
     >>> print(df.count())
-    1
+    169
     """
-    f_simbad = example_filter_(cdsxmatch, magpsf)
+    f_simbad = example_filter_(magpsf)
 
     return f_simbad
-
 
 if __name__ == "__main__":
     """Execute the test suite"""
