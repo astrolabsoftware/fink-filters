1f51ef5da0b472f76fa0187eba2960d96c0bf652
Emille Ishida <emilleishida@users.noreply.github.com>
2026-02-23 12:48:43 +0100

    [Rubin] Fixing Rubin early snia giving flat things (#341)
    * Implement extract_min_flux function
    
    Add function to extract minimum psfFluxMin across bands.
    
    * Refactor early_snia_candidate function parameters and logic
    
    Updated early_snia_candidate function to include diaObject parameter and modified selection criteria for SN Ia candidates.
    
    * remove white spaces
    
    * add missing numpy import and expand description
    
    * remove assert in tests, now it is more complicated
    
    * Fix description and update delta_mag to flux_ratio

--- fink_filters/rubin/livestream/filter_early_snia_candidate/filter.py
+++ fink_filters/rubin/livestream/filter_early_snia_candidate/filter.py
@@ -12,39 +12,50 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Select alerts with a early SN Ia classifier score above 0.5. See https://arxiv.org/abs/2404.08798."""
+"""Select alerts with a early SN Ia classifier score above 0.76 OR those with socre above 0.5 and log10(delta_flux) above 0.5. See https://arxiv.org/abs/2404.08798 for classifier algorithm."""
 
+import numpy as np
 import pandas as pd
+import fink_filters.rubin.utils as fu
 
-DESCRIPTION = "Select alerts with a early SN Ia classifier score above 0.5. See https://arxiv.org/abs/2404.08798."
+DESCRIPTION = "Select alerts with a early SN Ia classifier score above 0.76 OR those with score above 0.5 and log10(ratio_flux) above 0.5. See https://arxiv.org/abs/2404.08798 for classifier algorithm."
 
 
-def early_snia_candidate(earlySNIa_score: pd.Series) -> pd.Series:
-    """Select alerts with a early SN Ia classifier score above 0.5. See https://arxiv.org/abs/2404.08798.
+def early_snia_candidate(
+    earlySNIa_score: pd.Series, diaObject: pd.DataFrame
+) -> pd.Series:
+    """Select alerts using the early SN Ia classifier. See https://arxiv.org/abs/2404.08798.
 
     Parameters
     ----------
     earlySNIa_score: pd.Series
         Score (0...1) from the early SN Ia classifier
+    diaObject: pd.DataFrame
+        Full diaObject section of alerts. Must contain columns
+        {band}_psfFluxMin and {band}_psfFluxMax for bands
 
     Returns
     -------
     out: pd.Series of booleans
-        True if score above 0.5. False otherwise
+        True if score > 0.76 OR score > 0.5 & log10(flux_ratio) > 0.5.
 
     Examples
     --------
-    >>> s = pd.Series([0.6, 0.1, None])
-    >>> out = early_snia_candidate(s)
-    >>> assert out.sum() == 1, out.sum()
-
     >>> from fink_filters.rubin.utils import apply_block
     >>> df2 = apply_block(df, "fink_filters.rubin.livestream.filter_early_snia_candidate.filter.early_snia_candidate")
     >>> df2.count()
     0
     """
-    f_early_snia = earlySNIa_score > 0.5
-    return f_early_snia
+    # calculate log flux ratio
+    flux_ratio = np.log10(
+        fu.extract_max_flux(diaObject) / fu.extract_min_flux(diaObject)
+    )
+
+    f_flux_ratio = flux_ratio > 0.5
+    f_good_early_snia = earlySNIa_score > 0.76
+    f_medium_early_snia = np.logical_and(earlySNIa_score > 0.5, f_flux_ratio)
+
+    return np.logical_or(f_good_early_snia, f_medium_early_snia)
 
 
 if __name__ == "__main__":
