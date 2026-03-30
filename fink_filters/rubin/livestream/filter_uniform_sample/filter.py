00d96242461f96553d3ef9a065af858b6201a098
Mohammed Chamma <hms.mohammed@gmail.com>
2026-03-30 11:17:01 -0400

    Apply suggestions from code review
    * add licence header
    * s/get_random_alert/uniform_sample to match directory structure
    
    Co-authored-by: Julien <peloton@lal.in2p3.fr>

--- fink_filters/rubin/livestream/filter_uniform_sample/filter.py
+++ fink_filters/rubin/livestream/filter_uniform_sample/filter.py
@@ -1,9 +1,23 @@
-"Select 1% of all live alerts in a uniformly random way"
+# Copyright 2026 AstroLab Software
+# Author: Mohammed Chamma
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Select 1% of all live alerts in a uniformly random way"""
 import pandas as pd
 
 DESCRIPTION = "Select 1% of all live alerts in a uniformly random way"
 
-def get_random_alert(diaSourceId: pd.Series) -> pd.Series:
+def uniform_sample(diaSourceId: pd.Series) -> pd.Series:
     """
 
     Examples
@@ -14,7 +28,7 @@ def get_random_alert(diaSourceId: pd.Series) -> pd.Series:
     >>> # df = pd.read_parquet('dataset/rubin_test_data_10_0.parquet')
     >>> # df = spark.read.format('parquet').load('dataset/rubin_test_data_10_0.parquet')
     >>> totalcount = df.count()
-    >>> f = 'fink_filters.rubin.livestream.filter_uniform_sample.get_random_alert'
+    >>> f = 'fink_filters.rubin.livestream.filter_uniform_sample.uniform_sample'
     >>> df = apply_user_defined_filter(df, f)
     >>> ratio = df.count() / totalcount
     >>> (0.005 <= ratio < 0.02) # check we are between 0.5% and 2%
