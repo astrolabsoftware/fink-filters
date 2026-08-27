f4589f68876667b9259a18308ba5adeae0dbb291
Julien <peloton@lal.in2p3.fr>
2026-08-27 10:31:51 +0200

    [ZTF] update user-agent when downloading dustmaps from Harvard Dataverse (#408)
    * Update the user-agent for dustmaps query
    
    * Add a test for early TDE
    
    * Update doctest output

--- fink_filters/ztf/filter_early_tde_candidates/filter.py
+++ fink_filters/ztf/filter_early_tde_candidates/filter.py
@@ -338,6 +338,14 @@ def early_tde_candidates(
     -------
     out: Pandas DataFrame
         Pandas DataFrame with scores and parameters for the selected candidates
+
+    Examples
+    --------
+    >>> df = spark.read.format("parquet").load("datatest/regular")
+    >>> candidates = early_tde_candidates(df)  # doctest:+ELLIPSIS
+    Downloading SFD data file to /tmp/sfd/SFD_dust_4096_ngp.fits
+    ...
+    Downloading https://dataverse.harvard.edu/api/access/datafile/2902695 ...
     """
     if prefiltered is None:
         # Apply preliminary filtering
